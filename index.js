c2e0595b8c2eccd023d4a23ef988d49a3ee7d717
David Friedman <dfried514@gmail.com>
2017-11-13 15:24:14 -0800

    added and tested isWinner method to determine if one player has won the game

---
+++ index.js
@@ -0,0 +1,49 @@
+// var board = [
+//   null, null, null,
+//   null, null, null,
+//   null, null, null
+// ];
+
+var isWinner = player => {
+  var count = 0;
+
+  for (var i = 0; i <= 2; i++) {
+    for (var j = 0; j <= 2; j++) {
+      if (board[i][j] === player) {
+        count++;
+      }
+    }
+    if (count === 3) {
+      return true;
+    }
+    count = 0;
+  }
+  for (var k = 0; k <= 2; k++) {
+    for (var l = 0; l <= 2; l++) {
+      if (board[l][k] === player) {
+        count++;
+      }
+    }
+    if (count === 3) {
+      return true;
+    }
+    count = 0;
+  }
+  if (board[1][1] === player) {
+
+    if ((board[0][0] === player
+      && board[2][2] === player)
+      || (board[2][0] === player
+        && board[0][2] === player)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var board = [
+  ['X', 'O', 'O'],
+  ['X', 'X', 'O'],
+  ['O', 'X', 'X']
+];
+
